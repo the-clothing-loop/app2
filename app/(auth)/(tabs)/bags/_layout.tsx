fb447bc0094476bcea6469a537448833780c35ab
Lucian I. Last <li@last.nl>
2025-04-06 23:50:48 +0200

    bag creation only on host

--- app/(auth)/(tabs)/bags/_layout.tsx
+++ app/(auth)/(tabs)/bags/_layout.tsx
@@ -1,9 +1,12 @@
 import { Text } from "@/components/ui/text";
+import { authStoreAuthUserRoles } from "@/store/auth";
+import { useStore } from "@tanstack/react-store";
 import { Link, Stack } from "expo-router";
 import { useTranslation } from "react-i18next";
 
 export default function BagsStackLayout() {
   const { t } = useTranslation();
+  const authUserRoles = useStore(authStoreAuthUserRoles);
   return (
     <Stack
       screenOptions={{
@@ -15,13 +18,14 @@ export default function BagsStackLayout() {
         name="index"
         options={{
           title: t("bags"),
-          headerRight: () => (
-            <Link href="./bags/create" className="px-2">
-              <Text size="xl" className="text-primary-500">
-                {t("create")}
-              </Text>
-            </Link>
-          ),
+          headerRight: () =>
+            authUserRoles.isHost ? (
+              <Link href="./bags/create" className="px-2">
+                <Text size="xl" className="text-primary-500">
+                  {t("create")}
+                </Text>
+              </Link>
+            ) : undefined,
         }}
       />
       <Stack.Screen

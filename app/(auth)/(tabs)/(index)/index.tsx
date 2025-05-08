34f5a7289d115a25cdb335b1536adec1cbd32c11
Lucian I. Last <li@last.nl>
2025-05-08 15:02:51 +0200

    remove old console log

--- app/(auth)/(tabs)/(index)/index.tsx
+++ app/(auth)/(tabs)/(index)/index.tsx
@@ -128,7 +128,6 @@ export default function HomeScreen(props: {}) {
     }));
   }, [rulesCustom, rulesDefault]);
 
-  console.log("render rules page");
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"

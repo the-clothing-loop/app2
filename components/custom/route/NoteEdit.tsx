73efa89d39d43ef6f78592b01faf4edf24591234
Lucian I. Last <li@last.nl>
2025-05-30 12:22:00 +0200

    Make notes like the old app

--- components/custom/route/NoteEdit.tsx
+++ components/custom/route/NoteEdit.tsx
@@ -1,26 +1,22 @@
 import { HStack } from "@/components/ui/hstack";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import FormattedText from "../FormattedText";
 import { Box } from "@/components/ui/box";
-import { Button, ButtonText } from "@/components/ui/button";
 import { UID } from "@/api/types";
 import { chainChangeUserNote, chainGetUserNote } from "@/api/chain";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
-
-import { useDebounceCallback } from "usehooks-ts";
 import { Textarea, TextareaInput } from "@/components/ui/textarea";
-import { useForm } from "@tanstack/react-form";
+import { useInterval } from "usehooks-ts";
 
 export default function NoteEdit(props: {
   currentChainUid: undefined | UID;
   thisUserUid: undefined | UID;
   isNoteEditable: boolean;
 }) {
-  const { t } = useTranslation();
-  const [showNoteEdit, setShowNoteEdit] = useState(false);
+  const [note, setNote] = useState("");
   const queryNote = useQuery({
     queryKey: [
       "auth",
@@ -28,11 +24,24 @@ export default function NoteEdit(props: {
       props.currentChainUid,
       props.thisUserUid,
     ],
-    queryFn() {
-      return chainGetUserNote(props.currentChainUid!, props.thisUserUid!);
+    async queryFn() {
+      const res = await chainGetUserNote(
+        props.currentChainUid!,
+        props.thisUserUid!,
+      );
+      return res;
     },
     enabled: Boolean(props.currentChainUid && props.thisUserUid),
   });
+  const [isInitialNote, setIsInitialNote] = useState(true);
+  useEffect(() => {
+    if (queryNote.data === undefined) return;
+    if (isInitialNote) {
+      setIsInitialNote(false);
+      setNote(queryNote.data!);
+    }
+  }, [queryNote.data, isInitialNote]);
+
   const mutateNote = useMutation({
     async mutationFn(note: string) {
       if (!props.thisUserUid) throw "not logged in";
@@ -46,75 +55,59 @@ export default function NoteEdit(props: {
       });
     },
   });
-  const form = useForm({
-    defaultValues: {
-      note: "",
+  const { t } = useTranslation();
+  const [isFocused, setIsFocused] = useState(false);
+  useInterval(
+    () => {
+      if (queryNote.data !== note) {
+        mutateNote.mutateAsync(note);
+      }
     },
-    onSubmit({ value }) {
-      return mutateNote.mutateAsync(value.note);
-    },
-  });
-  const delayedFormSubmit = useDebounceCallback(() => {
-    form.handleSubmit();
-  }, 3000);
-  function toggleShowNoteEdit() {
-    const open = !showNoteEdit;
-    setShowNoteEdit(open);
-    if (open) {
-      form.setFieldValue("note", queryNote.data || "", {
-        dontUpdateMeta: true,
-      });
-    } else {
-      delayedFormSubmit.flush();
-    }
-  }
-  const isDirty = useMemo(
-    () => queryNote.data !== form.state.values.note,
-    [form.state.values.note, queryNote],
+    isFocused ? 3000 : null,
   );
 
+  const isDirty = useMemo(() => {
+    const a = queryNote.data;
+    const b = note;
+    return a !== b;
+  }, [queryNote.data, note]);
+
+  function onFocus() {
+    setIsFocused(true);
+  }
+  function onBlur() {
+    setIsFocused(false);
+  }
+
   return (
-    <HStack className="gap-2 px-4 py-2">
-      <VStack className="flex-grow">
+    <HStack className="shrink gap-2 px-4 py-2">
+      <VStack className="shrink flex-grow gap-0.5">
         <Text bold size="sm">
           {t("notes")}
         </Text>
-        {showNoteEdit ? (
+        {props.isNoteEditable ? (
           <Box className="relative">
             <Box
-              className={`absolute right-2 top-2 z-10 h-4 w-4 rounded-full ${isDirty ? "bg-warning-300" : "bg-success-300"}`}
+              className={`absolute right-2 top-2 z-10 h-4 w-4 rounded-full ${isInitialNote ? "bg-gray-500" : isDirty ? "bg-warning-300" : "bg-success-300"}`}
             />
-            <form.Field
-              name="note"
-              listeners={{
-                onChange() {
-                  delayedFormSubmit();
-                },
-              }}
-            >
-              {(field) => (
-                <Textarea>
-                  <TextareaInput
-                    value={field.state.value}
-                    onChangeText={field.setValue}
-                    onBlur={() => delayedFormSubmit()}
-                    submitBehavior="newline"
-                  />
-                </Textarea>
-              )}
-            </form.Field>
+            <Textarea className={`z-0 ${isDirty ? "!border-amber-600" : ""}`}>
+              <TextareaInput
+                className={` ${isInitialNote ? "bg-gray-200" : ""}`}
+                value={note}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onChangeText={setNote}
+                editable={!isInitialNote}
+                submitBehavior="newline"
+              />
+            </Textarea>
           </Box>
         ) : (
-          <FormattedText content={queryNote.data || t("empty")} />
+          <Box className="min-h-28 shrink rounded border border-background-300 p-2 data-[focus=true]:border-primary-700 data-[hover=true]:border-outline-400">
+            <FormattedText allowLinks content={queryNote.data || ""} />
+          </Box>
         )}
       </VStack>
-      <Box>
-        {props.isNoteEditable ? (
-          <Button size="sm" onPress={toggleShowNoteEdit}>
-            <ButtonText>{showNoteEdit ? t("save") : t("edit")}</ButtonText>
-          </Button>
-        ) : null}
-      </Box>
     </HStack>
   );
 }
